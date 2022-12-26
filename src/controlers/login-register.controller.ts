baf93ff62f16aec890923c21ce0a5aad73eb2e84
visitkoushik <visit.koushik@gmail.com>
2022-12-26 07:17:15 +0530

    Implemented change password, logout and login

--- src/controlers/login-register.controller.ts
+++ src/controlers/login-register.controller.ts
@@ -1,15 +1,49 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 import { Login } from 'src/models/Login';
 import { LoginRegisterService } from '../services/login-register.service';
 
-@Controller('Login')
+@Controller('auth')
 export class LoginRegisterController {
   constructor(private readonly moduleService: LoginRegisterService) {}
 
-  @Post('add')
-  addCategory(@Body() login: Login): any {
-    login.active = true;
-    const generatedResult = this.moduleService.insert(login);
-    return generatedResult;
+  @Post('/login')
+  async login(@Res() response: Response, @Req() request: Request) {
+    try {
+      response
+        .status(HttpStatus.OK)
+        .json(await this.moduleService.login(request.body));
+    } catch (e) {
+      response.status(HttpStatus.UNAUTHORIZED).json(e.message);
+    }
+  }
+  @Post('/logout')
+  async logout(@Res() response: Response, @Req() request: Request) {
+    try {
+      response
+        .status(HttpStatus.OK)
+        .json(await this.moduleService.logOut(request.body));
+    } catch (e) {
+      response.status(HttpStatus.UNAUTHORIZED).json(e.message);
+    }
+  }
+
+  @Post('/changepassword')
+  async changePass(@Res() response: Response, @Req() request: Request) {
+    try {
+      response
+        .status(HttpStatus.OK)
+        .json(await this.moduleService.changePass(request.body));
+    } catch (e) {
+      response.status(HttpStatus.UNAUTHORIZED).json(e.message);
+    }
   }
 }
--- src/services/login-register.service.ts
+++ src/services/login-register.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Login } from 'src/models/Login';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class LoginRegisterService {
@@ -18,5 +19,95 @@ export class LoginRegisterService {
       throw new Error(e.message);
     });
     return savedObject;
+  }
+  async logOut(authorization: { authcode: string }) {
+    const filter_stage = {
+      authCode: {
+        $eq: authorization.authcode,
+      },
+    };
+    try {
+      const logInfo: Login = await this.loginModel.findOne(filter_stage);
+      if (!logInfo) {
+        throw new Error('Invalid authcode');
+      }
+      logInfo.authCode = '';
+
+      const newObj: any = new this.loginModel(logInfo);
+      const savedObject = await newObj.save().catch((e) => {
+        throw new Error(e.message);
+      });
+      if (!savedObject) {
+        throw new Error(`Can't able to logout`);
+      }
+      return 'Successfuly Logout';
+    } catch (e) {
+      throw e;
+    }
+  }
+  async login(authorization: {
+    username: string;
+    password: string;
+  }): Promise<Login> {
+    const filter_stage = {
+      userName: {
+        $eq: authorization.username,
+      },
+      passcode: {
+        $eq: authorization.password,
+      },
+    };
+    try {
+      const logInfo: Login = await this.loginModel.findOne(filter_stage);
+      if (!logInfo) {
+        throw new Error('Invalid Login');
+      }
+      logInfo.authCode = uuid().replace(/-/g, '');
+
+      const newObj: any = new this.loginModel(logInfo);
+      const savedObject = await newObj.save().catch((e) => {
+        throw new Error(e.message);
+      });
+      if (!savedObject) {
+        throw new Error(`Can't able to login`);
+      }
+      return logInfo;
+    } catch (e) {
+      throw e;
+    }
+  }
+  async changePass(passObj: {
+    oldPassword: string;
+    newPassword: string;
+    authcode: string;
+  }) {
+    const filter_stage = {
+      authCode: {
+        $eq: passObj.authcode,
+      },
+    };
+
+    try {
+      const logInfo: Login = await this.loginModel.findOne(filter_stage);
+      if (!logInfo) {
+        throw new Error('Invalid authcode');
+      }
+      if (logInfo.passcode !== passObj.oldPassword) {
+        throw new Error('Old Password is not matching');
+      }
+      logInfo.authCode = '';
+      logInfo.passcode = passObj.newPassword;
+
+      const newObj: any = new this.loginModel(logInfo);
+      const savedObject = await newObj.save().catch((e) => {
+        throw new Error(e.message);
+      });
+      if (!savedObject) {
+        throw new Error(`Can't able to change password`);
+      }
+      return 'Successfuly changed password. Login to continue';
+    } catch (e) {
+      throw e;
+    }
   }
 }
